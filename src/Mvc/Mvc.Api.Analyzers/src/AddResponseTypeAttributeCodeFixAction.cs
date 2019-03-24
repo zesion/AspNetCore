a680a366cd24936a811e45a7af3ff69ac7d76e30
Ryan Nowak <rynowak@microsoft.com>
2019-03-24 14:53:08 -0700

    Refactor of Startup analyzer code

--- src/Mvc/Mvc.Api.Analyzers/src/AddResponseTypeAttributeCodeFixAction.cs
+++ src/Mvc/Mvc.Api.Analyzers/src/AddResponseTypeAttributeCodeFixAction.cs
@@ -37,6 +37,8 @@ namespace Microsoft.AspNetCore.Mvc.Api.Analyzers
             _diagnostic = diagnostic;
         }
 
+        public override string EquivalenceKey => base.EquivalenceKey;
+
         public override string Title => "Add ProducesResponseType attributes.";
 
         protected override async Task<Document> GetChangedDocumentAsync(CancellationToken cancellationToken)
