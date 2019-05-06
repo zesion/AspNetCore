1c39b35f4bfab04b6e3f50f1c831f366e54c85f1
Ryan Brandenburg <rybrande@microsoft.com>
2019-05-06 14:06:11 -0700

    Add Globbing tests

--- src/Tools/dotnet-openapi/test/OpenApiAddTests.cs
+++ src/Tools/dotnet-openapi/test/OpenApiAddTests.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -17,6 +18,12 @@ namespace Microsoft.DotNet.OpenApi.Add.Tests
     public class OpenApiAddTests : OpenApiTestBase
     {
         public OpenApiAddTests(ITestOutputHelper output) : base(output){}
+
+        [Fact]
+        public void OpenApi_Add_GlobbingOpenApi()
+        {
+            throw new NotImplementedException();
+        }
 
         [Fact]
         public async Task OpenApi_Add_ReuseItemGroup()
