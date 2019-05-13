0cc01a4da9655c52dfdddc509e9003990e73fa4a
Ryan Brandenburg <rybrande@microsoft.com>
2019-05-13 15:38:15 -0700

    Sign Build locator

--- src/Tools/dotnet-openapi/src/Commands/BaseCommand.cs
+++ src/Tools/dotnet-openapi/src/Commands/BaseCommand.cs
@@ -3,11 +3,8 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Net.Http;
-using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Build.Evaluation;
